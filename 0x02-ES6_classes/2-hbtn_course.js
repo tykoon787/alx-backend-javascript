28b61baf6527e6093706142f907e0ea51cc1faeb
tykoon787 <tykoon787@protonmail.com>
2023-07-15 14:22:05 +0300

    Updated code

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,31 +1,58 @@
 export default class HolbertonCourse {
     constructor(name, length, students) {
-        this._name = name;
-        this._length = length;
-        this._students = students;
+        if (typeof name === "string") {
+            this._name = name;
+        } else {
+            throw new Error("Invalid Type. Expected String")
+        }
+
+        if (typeof length === "number") {
+            this._length = length;
+        } else {
+            throw new Error("Invalid Type. Expected Number")
+        }
+
+
+        if (Array.isArray(students)) {
+            this._students = students
+        } else {
+            throw new Error("Invalid Type. Expected Students")
+        }
     }
 
     get name() {
         return this._name;
     }
 
-    set name(name) {
-        this._name = name
+    set name(newName) {
+        if (typeof newName === "string") {
+            this._name = newName;
+        } else {
+            throw new Error("Invalid Type. Expected String")
+        }
     }
 
     get length() {
         return this._length;
     }
 
-    set length(length) {
-        this._length = length;
+    set length(newLength) {
+        if (typeof newLength === "number") {
+            this._length = length;
+        } else {
+            throw new Error("Invalid Type. Expected Number")
+        }
     }
 
     get students() {
         return this._students
     }
 
-    set students(students) {
-        this._students = students
+    set students(newStudents) {
+        if (Array.isArray(newStudents)) {
+            this._students = newStudents
+        } else {
+            throw new Error("Invalid Type. Expected Array")
+        }
     }
 }
