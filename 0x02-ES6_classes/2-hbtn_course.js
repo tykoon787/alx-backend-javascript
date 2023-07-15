cc560b5d1ba75fd3d6daf97c75b4429be30e3ff2
tykoon787 <tykoon787@protonmail.com>
2023-07-15 21:25:43 +0300

    Updated code

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,58 +1,57 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        if (typeof name === "string") {
-            this._name = name;
-        } else {
-            throw new Error("Invalid Type. Expected String")
-        }
-
-        if (typeof length === "number") {
-            this._length = length;
-        } else {
-            throw new Error("Invalid Type. Expected Number")
-        }
-
-
-        if (Array.isArray(students)) {
-            this._students = students
-        } else {
-            throw new Error("Invalid Type. Expected Students")
-        }
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('Invalid Type. Expected String');
     }
 
-    get name() {
-        return this._name;
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new Error('Invalid Type. Expected Number');
     }
 
-    set name(newName) {
-        if (typeof newName === "string") {
-            this._name = newName;
-        } else {
-            throw new Error("Invalid Type. Expected String")
-        }
+    if (Array.isArray(students)) {
+      this._students = students;
+    } else {
+      throw new Error('Invalid Type. Expected Students');
     }
+  }
 
-    get length() {
-        return this._length;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set length(newLength) {
-        if (typeof newLength === "number") {
-            this._length = length;
-        } else {
-            throw new Error("Invalid Type. Expected Number")
-        }
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new Error('Invalid Type. Expected String');
     }
+  }
 
-    get students() {
-        return this._students
-    }
+  get length() {
+    return this._length;
+  }
 
-    set students(newStudents) {
-        if (Array.isArray(newStudents)) {
-            this._students = newStudents
-        } else {
-            throw new Error("Invalid Type. Expected Array")
-        }
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
+      throw new Error('Invalid Type. Expected Number');
     }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents)) {
+      this._students = newStudents;
+    } else {
+      throw new Error('Invalid Type. Expected Array');
+    }
+  }
 }
