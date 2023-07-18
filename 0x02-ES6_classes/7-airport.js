ef23b6e09f76ea2b7614a24b75fc0146925212e5
tykoon787 <tykoon787@protonmail.com>
2023-07-18 10:35:25 +0300

    Updated code

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,19 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('(name) Invalid Type. Expected String');
+    }
+
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new Error('(code) Invalid Type. Expected String');
+    }
+
+    toString() {
+      return this._code;
+    }
+  }
+}
