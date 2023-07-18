56c76f013f4a3f8ebe447f610baf002deb191971
tykoon787 <tykoon787@protonmail.com>
2023-07-18 09:41:34 +0300

    Updated code

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,36 +1,24 @@
 export default class Building {
-    constructor(sqft) {
-        // Check if method has been overidden
-        if (this.evacuationWarningMessage !== 'function') {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
-
-        if (typeof sqft === 'number') {
-            this._sqft = sqft;
-        }
+  constructor(sqft) {
+    // Check if method has been overidden
+    if (this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
-    get sqft() {
-        return this._sqft;
+    if (typeof sqft === 'number') {
+      this._sqft = sqft;
     }
+  }
 
-    set sqft(newSqft) {
-        if (typeof newSqft === 'number') {
-            this._sqft = newSqft;
-        } else {
-            throw new Error('Invalid Type. (sqft) should be of type Number');
-        }
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft === 'number') {
+      this._sqft = newSqft;
+    } else {
+      throw new Error('Invalid Type. (sqft) should be of type Number');
     }
-}
-
-const b = new Building(100);
-console.log(b);
-
-class TestBuilding extends Building {}
-
-try {
-    new TestBuilding(200)
-}
-catch(err) {
-    console.log(err);
+  }
 }
