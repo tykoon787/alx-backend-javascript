b6763d92d324dcd2b773fa3b3f170d3ecefc2958
tykoon787 <tykoon787@protonmail.com>
2023-07-18 10:15:46 +0300

    Updated code

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -19,6 +19,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `${this._floors} floors`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
