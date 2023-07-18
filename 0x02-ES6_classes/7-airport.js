c64fd5cdacd9d8ff9cd2e84b6b7b9362b1aa6920
tykoon787 <tykoon787@protonmail.com>
2023-07-18 10:46:33 +0300

    Updated code

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -13,7 +13,35 @@ export default class Airport {
     }
   }
 
-  toString() {
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new Error('(name) Invalid Type. Expected String');
+    }
+  }
+
+  get code() {
     return this._code;
   }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    } else {
+      throw new Error('(code) Invalid Type. Expected String');
+    }
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
 }
+
+const airportSF = new Airport('San Francisco Airport', 'SFO');
+console.log(airportSF);
+console.log(airportSF.toString());
